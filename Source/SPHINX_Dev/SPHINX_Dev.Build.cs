79356083df4f853d4e915a1ad32d6f152b947599
sdavern <74747945+sdavern@users.noreply.github.com>
2024-07-15 16:44:44 +0100

    Commit 150724

--- Source/SPHINX_Dev/SPHINX_Dev.Build.cs
+++ Source/SPHINX_Dev/SPHINX_Dev.Build.cs
@@ -8,7 +8,7 @@ public class SPHINX_Dev : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AssetRegistry", "Paper2D", "PaperZD" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AssetRegistry", "Paper2D", "PaperZD", "Slate", "SlateCore", "EditorScriptingUtilities", "Blutility", "UMG", "UMGEditor", "UnrealEd" });
 		
 	}
 }
